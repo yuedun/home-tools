809d1f373d51d5107814f2a7dd240c3299844c4e
yuedun <light_v@163.com>
2018-02-18 16:47:25 +0800

    heroku部署

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var ejs = require("ejs");
 var path = require("path");
 var app = express();
 var request = require("request");
+var favicon = require("serve-favicon");
 var SocketServer = require("./socket-server.js");
 
 // view engine setup环境变量设置
@@ -10,6 +11,7 @@ app.set('views', __dirname);
 app.engine('.html', ejs.renderFile);
 app.set('view engine', 'html');
 app.use(express.static(__dirname));
+app.use(favicon(path.join(__dirname, './logo.png')));
 
 /**
  * 首页
@@ -30,11 +32,14 @@ app.get('/sendMsg', function (req, res) {
  */
 app.get('/getWeather', function (req, res) {
     request("http://service.envicloud.cn:8082/v2/weatherforecast/EXVLZHVUMTQ3NDYXNZE2NTY0NG==/101020100",
-    function (error, response, body) {
-        res.send(body)
-    })
+        function (error, response, body) {
+            res.send(body)
+        })
 })
-var server = app.listen(3000);
+var port = process.env.PORT || 3000;
+var server = app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+});
 
 var io = require("socket.io")(server);
 io.on('connection', function (socket) {
